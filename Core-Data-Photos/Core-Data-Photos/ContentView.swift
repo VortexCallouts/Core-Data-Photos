77e816ea2f6e50e745e3bacfb9d7434b550acf9c
VortexCallouts <ftvortex1@gmail.com>
2019-10-25 11:02:42 -0500

    Added Xcode Project

---
+++ Core-Data-Photos/Core-Data-Photos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Core-Data-Photos
+//
+//  Created by Davin Heilich on 10/25/19.
+//  Copyright © 2019 Davin Heilich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
